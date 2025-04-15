201c08a7e65fed9323e6dae8232be7d9329fde5b
Kirill <153691801+Xutrikk@users.noreply.github.com>
2025-04-15 20:22:37 +0300

    Add files via upload

--- TPVI/lab2/Program.cs
+++ TPVI/lab2/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Map("/aspnetcore", subApp =>
-{
-    subApp.UseWelcomePage();
-});
-app.UseDefaultFiles();
+var defaultFilesOptions = new DefaultFilesOptions();
+defaultFilesOptions.DefaultFileNames.Clear();
+defaultFilesOptions.DefaultFileNames.Add("Neumann.html");
+
+app.UseDefaultFiles(defaultFilesOptions);
 app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "Picture")),
+    RequestPath = "/static"
+});
+
+app.MapGet("/aspnetcore", () => "Добро пожаловать в ASP.NET Core!");
 
 app.Run();
--- TPVI/lab2/calc.js
+++ TPVI/lab2/calc.js
@@ -1,7 +1,10 @@
-﻿document.getElementById("sum").textContent = Sum(7, 3);
-document.getElementById("sub").textContent = Sub(7, 3);
-document.getElementById("mul").textContent = Mul(7, 3);
-document.getElementById("div").textContent = Div(7, 3).toFixed(2);
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("sum").textContent = Sum(7, 3);
+    document.getElementById("sub").textContent = Sub(7, 3);
+    document.getElementById("mul").textContent = Mul(7, 3);
+    document.getElementById("div").textContent = Div(7, 3).toFixed(2);
+});
+
 function Sum(a, b) { return a + b; }
 function Sub(a, b) { return a - b; }
 function Mul(a, b) { return a * b; }
