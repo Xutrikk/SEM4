e327002398217eb6f0c8d1f7f3cfaed668c6f3cd
Kirill <153691801+Xutrikk@users.noreply.github.com>
2025-04-15 20:29:06 +0300

    Add files via upload

---
+++ OOP/lab1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ConstructionCalculator
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private TextBox txtLength;
+        private TextBox txtWidth;
+        private TextBox txtHeight;
+        private ComboBox cmbMaterial;
+        private ComboBox cmbUnits;
+        private Button btnCalculate;
+        private TextBox txtResults;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtLength = new TextBox();
+            this.txtWidth = new TextBox();
+            this.txtHeight = new TextBox();
+            this.cmbMaterial = new ComboBox();
+            this.cmbUnits = new ComboBox();
+            this.btnCalculate = new Button();
+            this.txtResults = new TextBox();
+            this.label1 = new Label();
+            this.label2 = new Label();
+            this.label3 = new Label();
+            this.label4 = new Label();
+            this.label5 = new Label();
+
+            // Настройка формы
+            this.SuspendLayout();
+            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.Text = "Строительный калькулятор";
+
+            // Настройка элементов
+            // txtLength
+            this.txtLength.Location = new System.Drawing.Point(120, 20);
+            this.txtLength.Size = new System.Drawing.Size(150, 20);
+
+            // txtWidth
+            this.txtWidth.Location = new System.Drawing.Point(120, 60);
+            this.txtWidth.Size = new System.Drawing.Size(150, 20);
+
+            // txtHeight
+            this.txtHeight.Location = new System.Drawing.Point(120, 100);
+            this.txtHeight.Size = new System.Drawing.Size(150, 20);
+
+            // cmbMaterial
+            this.cmbMaterial.Location = new System.Drawing.Point(120, 140);
+            this.cmbMaterial.Size = new System.Drawing.Size(150, 21);
+
+            // cmbUnits
+            this.cmbUnits.Location = new System.Drawing.Point(120, 180);
+            this.cmbUnits.Size = new System.Drawing.Size(150, 21);
+
+            // btnCalculate
+            this.btnCalculate.Location = new System.Drawing.Point(120, 220);
+            this.btnCalculate.Size = new System.Drawing.Size(150, 30);
+            this.btnCalculate.Text = "Рассчитать";
+
+            // txtResults
+            this.txtResults.Multiline = true;
+            this.txtResults.Location = new System.Drawing.Point(20, 260);
+            this.txtResults.Size = new System.Drawing.Size(350, 150);
+            this.txtResults.ScrollBars = ScrollBars.Vertical;
+            this.txtResults.ReadOnly = true;
+
+            // Labels
+            this.label1.Text = "Длина:";
+            this.label1.Location = new System.Drawing.Point(20, 20);
+
+            this.label2.Text = "Ширина:";
+            this.label2.Location = new System.Drawing.Point(20, 60);
+
+            this.label3.Text = "Высота:";
+            this.label3.Location = new System.Drawing.Point(20, 100);
+
+            this.label4.Text = "Материал:";
+            this.label4.Location = new System.Drawing.Point(20, 140);
+
+            this.label5.Text = "Единицы:";
+            this.label5.Location = new System.Drawing.Point(20, 180);
+
+            // Добавление элементов на форму
+            this.Controls.AddRange(new Control[] {
+                txtLength, txtWidth, txtHeight,
+                cmbMaterial, cmbUnits,
+                btnCalculate, txtResults,
+                label1, label2, label3, label4, label5
+            });
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ OOP/lab1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConstructionCalculator
+{
+    public partial class MainForm : Form
+    {
+      
+        public delegate double ConversionHandler(double value);
+        public delegate double CalculationHandler(double area);
+
+      
+        public event Action<string> CalculationStarted;
+        public event Action<string> CalculationCompleted;
+        public event Action<string> ErrorOccurred;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SubscribeEvents();
+            InitializeComponents();
+        }
+
+        private void SubscribeEvents()
+        {
+            btnCalculate.Click += BtnCalculate_Click;
+            CalculationStarted += OnCalculationStarted;
+            CalculationCompleted += OnCalculationCompleted;
+            ErrorOccurred += OnErrorOccurred;
+        }
+
+        private void InitializeComponents()
+        {
+            cmbMaterial.Items.AddRange(new object[] { "Ламинат", "Плитка", "Обои" });
+            cmbUnits.Items.AddRange(new object[] { "Метры", "Футы" });
+            cmbMaterial.SelectedIndex = 0;
+            cmbUnits.SelectedIndex = 0;
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CalculationStarted?.Invoke("Начало расчета...");
+
+                var input = GetInputValues();
+                ValidateInput(input);
+
+                var converter = GetConverter();
+                var calculator = GetCalculator();
+
+                var results = CalculateResults(input, converter, calculator);
+
+                DisplayResults(results);
+                CalculationCompleted?.Invoke("");
+            }
+            catch (Exception ex)
+            {
+                ErrorOccurred?.Invoke(ex.Message);
+            }
+        }
+
+        private InputData GetInputValues()
+        {
+            return new InputData(
+                Length: double.Parse(txtLength.Text),
+                Width: double.Parse(txtWidth.Text),
+                Height: double.Parse(txtHeight.Text),
+                Material: cmbMaterial.SelectedItem.ToString(),
+                Units: cmbUnits.SelectedIndex
+            );
+        }
+
+        private void ValidateInput(InputData data)
+        {
+            if (data.Length <= 0 || data.Width <= 0 || data.Height <= 0)
+                throw new ArgumentException("Все размеры должны быть положительными числами");
+        }
+
+        private ConversionHandler GetConverter()
+        {
+            return cmbUnits.SelectedIndex == 0
+                ? new ConversionHandler(ConvertToSquareMeters)
+                : new ConversionHandler(ConvertToSquareFeet);
+        }
+
+        private CalculationHandler GetCalculator()
+        {
+            return cmbMaterial.SelectedIndex switch
+            {
+                0 => CalculateLaminate,
+                1 => CalculateTiles,
+                2 => CalculateWallpaper,
+                _ => throw new ArgumentException("Неверный тип материала")
+            };
+        }
+
+        private CalculationResults CalculateResults(InputData data,
+                                                   ConversionHandler converter,
+                                                   CalculationHandler calculator)
+        {
+            double floorArea = data.Length * data.Width;
+            double wallsArea = 2 * (data.Length + data.Width) * data.Height;
+
+            return new CalculationResults(
+                FloorArea: converter(floorArea),
+                WallsArea: converter(wallsArea),
+                MaterialNeeded: calculator(data.Material == "Обои" ? wallsArea : floorArea)
+            );
+        }
+
+        private void DisplayResults(CalculationResults results)
+        {
+            string floorArea = $"Площадь пола: {results.FloorArea:F2} {GetUnitSymbol()}";
+            string wallsArea = $"Площадь стен: {results.WallsArea:F2} {GetUnitSymbol()}";
+            string materialNeeded = $"Требуется материала: {Math.Ceiling(results.MaterialNeeded)} {GetMaterialUnit()}";
+            string separator = new string('─', 30);
+
+            txtResults.AppendText(
+                string.Join(Environment.NewLine,
+                    Environment.NewLine, // Добавляем пустую строку
+                    floorArea,
+                    wallsArea,
+                    materialNeeded,
+                    separator,
+                    "Расчет завершен!")
+            );
+        }
+
+        // Конверсионные методы
+        private double ConvertToSquareFeet(double meters) => meters * 10.7639;
+        private double ConvertToSquareMeters(double feet) => feet;
+
+        // Методы расчета материалов
+        private double CalculateLaminate(double area) => area * 1.1 / 2.5;
+        private double CalculateTiles(double area) => area * 1.15 / 1.8;
+        private double CalculateWallpaper(double area) => area * 1.2 / 5.0;
+
+        // Вспомогательные методы
+        private string GetUnitSymbol() => cmbUnits.SelectedIndex == 0 ? "м²" : "фут²";
+        private string GetMaterialUnit() => cmbMaterial.SelectedIndex == 2 ? "рулонов" : "упаковок";
+
+        // Обработчики событий
+        private void OnCalculationStarted(string message)
+        {
+            txtResults.Text = message;
+            btnCalculate.Enabled = false;
+        }
+
+        private void OnCalculationCompleted(string message)
+        {
+            txtResults.AppendText($"\n{message}");
+            btnCalculate.Enabled = true;
+        }
+
+        private void OnErrorOccurred(string message)
+        {
+            MessageBox.Show(message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            btnCalculate.Enabled = true;
+        }
+
+        // Классы данных
+        private record InputData(
+            double Length,
+            double Width,
+            double Height,
+            string Material,
+            int Units
+        );
+
+        private record CalculationResults(
+            double FloorArea,
+            double WallsArea,
+            double MaterialNeeded
+        );
+    }
+}
