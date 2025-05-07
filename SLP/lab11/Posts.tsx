2901b7dd4a3990dc40cf02e062b891cef169bd8a
Kirill <153691801+Xutrikk@users.noreply.github.com>
2025-05-08 02:30:02 +0300

    Add files via upload

---
+++ SLP/lab11/Posts.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '../../app/store'; 
+import { fetchPostsThunk } from './postsSlice';
+import PostForm from '../../components/PostForm';
+import PostItem from '../../components/PostItem';
+import { Post } from './postsAPI';
+
+const Posts: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { posts, status, error } = useSelector((state: RootState) => state.posts);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchPostsThunk());
+    }
+  }, [status, dispatch]);
+
+  return (
+    <div>
+      <h1>Post Manager</h1>
+      <PostForm />
+      {status === 'loading' && <p data-loading>Загрузка...</p>}
+      {status === 'failed' && <p data-error>Ошибка: {error}</p>}
+      {status === 'succeeded' && (
+        <ul>
+          {posts.map((post: Post) => (
+            <PostItem key={post.id} post={post} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Posts;
